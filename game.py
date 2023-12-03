c2684b4eeb5e2446ac0a5c7f63c1ea32ad65b3aa
Wong Zheng Yew <wzy70207@gmail.com>
2023-12-03 14:12:10 +0800

    added total score

--- game.py
+++ game.py
@@ -31,9 +31,10 @@ def handle_backspace(state: dict):
     # TODO: Should decrease the cursor index by 1 if the cursor index is not 0
     if state["cursor_index"] != 0:
         state["cursor_index"] -= 1
+    else:
+        state["cursor_index"] = 0
     # TODO: Should not change the cursor index if the cursor index is 0
     # TODO: Should update the char_states based on the cursor index
-    state["char_states"] = is_key_correct(state["cursor_index"])
 
 
 def is_key_correct(key, state: dict):
@@ -44,45 +45,56 @@ def is_key_correct(key, state: dict):
 
 def handle_correct_key(key, state: dict):  # Mutates the state
     """Handle the correct key."""
-    # TODO: Should update the cursor index
-    cursor_index += 1
+    # if result of is_key_correct is true
+    if is_key_correct('key', state) == True:
+        # TODO: Should update the cursor index
+        state['cursor_index'] += 1
     # TODO: Update char_states based on the cursor index
-    char_states = 1
-    # TODO: Update score based on the cursor index
+        state['char_states'] = 1
 
 
 def handle_incorrect_key(key, state: dict):  # Mutates the state
     """Handle the incorrect key."""
-    # TODO: Should update the cursor index
-    cursor_index += 1
+    # if result of is_key_correct is incorrect
+    if is_key_correct == False:
+        # TODO: Should update the cursor index
+        state['cursor_index'] += 1
     # TODO: Update char_states based on the cursor index
-    char_states = 2
+        state['char_states'] = 2
 
 
 def on_key_press(key, _state: dict):
-    """Callback function for when a key is pressed."""
+    # Callback function for when a key is pressed.
     state = _state.copy()
-
+    """
     # TODO: Remove this example, which increments the score by 1 when any key is pressed, and displays the time since the previous keypress
-    state["score"] = _state["score"] + 1
-    previous_timestamp = _state["previous_timestamp"]
+    state["score"] = state["score"] + 1
+    previous_timestamp = state["previous_timestamp"]
     timestamp = time.time()
     delta_time = timestamp - previous_timestamp
-    print(state["score"], delta_time, key)
-
+    print(state["score"], delta_time)
+    """
     # Refer to main.py for the state key schema
+    # timestamp = time.time()
+    # state["score"] = state["score"] + 1
 
     if state["cursor_index"] == len(state["text"]):
         # TODO: Handle edge case when game is complete, i.e. cursor_index == len(text)
-        pass
-
+        return state['score']
     if is_backspace(key):
         handle_backspace(state["text"])
         # TODO: Handle edge case when backspace is pressed
-    if is_key_correct("text", state=dict):
+    if is_key_correct("text", state):
         handle_correct_key("text")
+        # state["previous_timestamp"] = timestamp
         # TODO: Check if the key pressed is the correct key based on the cursor index and the text, and handle it accordingly
-
         # Last step: Update the previous_timestamp key in the state
-        state["previous_timestamp"] = timestamp
+        # state["previous_timestamp"] = timestamp
         # return state
+    length = len(state["char_states"])
+    for i in range(length):
+        if i == 1:
+            state["score"] = state["score"] + i
+        else:
+            state["score"] = state["score"] + i
+    return state["score"]
